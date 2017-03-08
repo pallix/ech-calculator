8d4f2156be4cf75906cde59d65cfbf0c142ac343
Pierre Allix <pierre@iilab.org>
2017-03-08 12:02:00 +0100

    Set outdoor water usage to 425L a day

--- src/Calculator/Cleaning.purs
+++ src/Calculator/Cleaning.purs
@@ -14,6 +14,5 @@ buildTimeserie key intervals = \(TimeInterval { date, period }) -> case period o
   OneMonth -> let nbDays :: Days
                   nbDays = toDuration $ diff (nextMonth date) date
               in Just $ usagePerDay * (unwrap nbDays)
-  -- 7% of total water usage is for outdoors activities (garden, cleaning, water plays)buildTimeserie key intervals = flip lookup $ (foldr feed empty intervals)
-  -- we infer 3% is for cleaning
-  where usagePerDay = 150.0 * 0.03
+  -- UK Energy saving trust did a survey on water use at home and it stated that 1% of the daily water use (150L) is for car washing. The main use of water outdoor is to wash car and tools, in addition to gardening and water play. So we can assume that for the demand of water for outdoor cleaning is 1.5L per person per day. Meakin Estate has 123 flats and UK average household has 2.3 persons. In such way we could work out the daily demand is 1.5*123*2.3 = ~425L
+  where usagePerDay = 425.0
